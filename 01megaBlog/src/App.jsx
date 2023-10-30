f739d180349d0ed3c080def8725496d1ae900eb0
krtatos <kkushagra626@gmail.com>
2023-10-30 12:06:34 +0530

    setting up environment variable and dependencies

--- 01megaBlog/src/App.jsx
+++ 01megaBlog/src/App.jsx
@@ -6,7 +6,9 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
+
     <>
      <h1>megaBlog web-app with appWrite</h1>
      
