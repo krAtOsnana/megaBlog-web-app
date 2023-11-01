5d9eeaf72a6eba974abd2fd7aeef45fb6d1026e8
krtatos <kkushagra626@gmail.com>
2023-11-01 15:13:12 +0530

    midway

---
+++ 01megaBlog/src/components/Headder/Headder.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Headder() {
+  return (
+    <div>Headder</div>
+  )
+}
+
+export default Headder
---
+++ 01megaBlog/src/components/index.js
@@ -0,0 +1,8 @@
+import Headder from "./Headder/Headder";
+import Footer from "./Footer/Footer";
+
+
+export {
+    Headder,
+    Footer
+}
